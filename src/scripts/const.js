53c8c3872c6a3501660d3929fdeb15918b017162
janewu77 <conchwu@hotmail.com>
2024-04-16 00:53:21 +0800

    add: 查单词按钮

--- src/scripts/const.js
+++ src/scripts/const.js
@@ -12,11 +12,16 @@ const arrTTSModel = ["tts-1", "tts-1-hd"];
 const arrTTSVoice = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"];
 
 
-
-const default_prompt_word = `  
+const default_chat_endpoint = "https://api.openai.com/v1/chat/completions";
+const arrChatModel = ["gpt-3.5-turbo", "gpt-4-turbo"];
+const defalut_llm_model = "gpt-3.5-turbo";
+const default_action_prompt = `  
 - explain the word in detail in English
 - phonetic notation
 - common collocations or frequent word combinations
 - examples in English
 - explain the word in detail in Chinese
 `;
+const default_action_name = `查单词`;
+
+
